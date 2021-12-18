4be3e48e3af73e4a8c8a1d57120662edca05a0cc
Julien Desrosiers <juliend2@gmail.com>
2021-12-18 18:23:52 -0500

    compress the js code even more

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
 du:
-	du -bs --exclude=.git --exclude=bz2/LICENSE --exclude=Makefile --exclude=bible.txt
+	du -bs --exclude=.git --exclude=bz2/LICENSE --exclude=Makefile --exclude=bible.txt --exclude=script.js
 
 compile:
-	terser bz2/index.js -o bz2/index.min.js
+	# terser bz2/index.js -m -o bz2/index.min.js
+	terser script.js  -m -o script.min.js
 
 server:
 	python -m http.server 8000
--- script.js
+++ script.js
@@ -1,44 +1,82 @@
-const DEBUG = false
-const DEBUG_LENGHT = 312400
-// Download and decompress bzip2:
-if (!localStorage.getItem('txt', string)) {
-    let resp = await fetch('bible.txt.bz2')
-    let buff = await resp.arrayBuffer()
-    const decompressed = bz2.decompress(new Uint8Array(buff))
-    var string = new TextDecoder("utf-8").decode(decompressed)
-    localStorage.setItem('txt', string)
-} else {
-    var string = localStorage.getItem('txt', string)
-}
-// Decompress extra compression:
-var replaces = {'$s':'shall','$l':'lord','$L':'LORD','$t':'their','$T':'that','$o':'thou','$y':'they','$h':'have','$f':'from','$w':'when'};
-Object.keys(replaces).forEach(k=>{string=string.replace(new RegExp(`\\${k}`, 'g'),replaces[k])})
-var verseTokens = string.slice(0, DEBUG ? DEBUG_LENGHT : -1).split(/\n?(\d+:\d+) /g).slice(1)
-var verses = []
-for (var i=0;i<verseTokens.length;i++) {
-    if (i % 2 == 0) {
-        verses.push([verseTokens[i]])
+async function load(){
+    const DEBUG = false
+    const DEBUG_LENGHT = 312400
+    // Download and decompress bzip2:
+    if (!localStorage.getItem('txt', string)) {
+        let resp = await fetch('bible.txt.bz2')
+        let buff = await resp.arrayBuffer()
+        const decompressed = bz2.decompress(new Uint8Array(buff))
+        var string = new TextDecoder("utf-8").decode(decompressed)
+        localStorage.setItem('txt', string)
     } else {
-        verses[verses.length-1].push(verseTokens[i])
+        var string = localStorage.getItem('txt', string)
     }
-}
-const chapterTitles = ["Genesis","Exodus","Leviticus","Numbers","Deuteronomy","Joshua","Judges","Ruth","1 Samuel","2 Samuel","1 Kings","2 Kings","1 Chronicles","2 Chronicles","Ezra","Nehemiah","Esther","Job","Psalms","Proverbs","Ecclesiastes","Song of Solomon","Isaiah","Jeremiah","Lamentations","Ezekiel","Daniel","Hosea","Joel","Amos","Obadiah","Jonah","Micah","Nahum","Habakkuk","Zephaniah","Haggai","Zechariah","Malachi","Matthew","Mark","Luke","John","Acts","Romans","1 Corinthians","2 Corinthians","Galatians","Ephesians","Philippians","Colossians","1 Thessalonians","2 Thessalonians","1 Timothy","2 Timothy","Titus","Philemon","Hebrews","James","1 Peter","2 Peter","1 John","2 John","3 John","Jude","Revelation"]
-var books = []
-for (let i = 0; i < verses.length; i++) {
-    if (verses[i][0] == '1:1') { // starting a new book?
-        books.push([verses[i]])
-    } else {
-        books[books.length-1].push(verses[i])
+    // Decompress extra compression:
+    var replaces = {'$s':'shall','$l':'lord','$L':'LORD','$t':'their','$T':'that','$o':'thou','$y':'they','$h':'have','$f':'from','$w':'when'};
+    Object.keys(replaces).forEach(k=>{string=string.replace(new RegExp(`\\${k}`, 'g'),replaces[k])})
+    var verseTokens = string.slice(0, DEBUG ? DEBUG_LENGHT : -1).split(/\n?(\d+:\d+) /g).slice(1)
+    var verses = []
+    for (var i=0;i<verseTokens.length;i++) {
+        if (i % 2 == 0) {
+            verses.push([verseTokens[i]])
+        } else {
+            verses[verses.length-1].push(verseTokens[i])
+        }
     }
+    function getChapters(verses) {
+        var chapters = []
+        var currentChapter = 0
+        for (let i = 0; i < verses.length; i++) {
+            currentChapter = verses[i][0].split(':')[0]
+            if (chapters[chapters.length-1] == currentChapter) {
+                chapters[chapters.length-1].push([currentChapter, verses[i]])
+            } else {
+                chapters.push([currentChapter, verses[i]])
+            }
+        }
+        return chapters
+    }
+    const bookTitles = ["Genesis","Exodus","Leviticus","Numbers","Deuteronomy","Joshua","Judges","Ruth","1 Samuel","2 Samuel","1 Kings","2 Kings","1 Chronicles","2 Chronicles","Ezra","Nehemiah","Esther","Job","Psalms","Proverbs","Ecclesiastes","Song of Solomon","Isaiah","Jeremiah","Lamentations","Ezekiel","Daniel","Hosea","Joel","Amos","Obadiah","Jonah","Micah","Nahum","Habakkuk","Zephaniah","Haggai","Zechariah","Malachi","Matthew","Mark","Luke","John","Acts","Romans","1 Corinthians","2 Corinthians","Galatians","Ephesians","Philippians","Colossians","1 Thessalonians","2 Thessalonians","1 Timothy","2 Timothy","Titus","Philemon","Hebrews","James","1 Peter","2 Peter","1 John","2 John","3 John","Jude","Revelation"]
+    var books = []
+    for (let i = 0; i < verses.length; i++) {
+        if (verses[i][0] == '1:1') { // starting a new book?
+            books.push([verses[i]])
+        } else {
+            books[books.length-1].push(verses[i])
+        }
+    }
+    // Put chapters in books:
+    for (let i = 0; i < books.length; i++) {
+        var chapters = {}
+        books[i].forEach(verse => {
+            var chapterNumber = verse[0].split(':')[0]
+            if (chapterNumber in chapters) {
+                chapters[chapterNumber].push(verse)
+            } else {
+                chapters[chapterNumber] = [verse]
+            }
+        })
+        books[i] = chapters
+    }
+    window.book = function(bookIndex) {
+        window.currentBookIndex = bookIndex
+        render(bookIndex, 0)
+    }
+    window.chapter= function(chapterIndex) {
+        window.currentChapterIndex = chapterIndex
+        render(window.currentBookIndex, chapterIndex)
+    }
+    function render(bookIndex, chapterIndex=0) {
+        const d = document
+        var body = d.querySelector('body')
+        body.innerHTML = `
+            <select onchange="book(this.value)">${books.map((_,i)=>`<option value="${i}" ${bookIndex==i?'selected':''}>${bookTitles[i]}</option>`).join('')}</select>
+            <select onchange="chapter(this.value)">${Object.keys(books[bookIndex]).map((ci,i)=>`<option value="${i}" ${chapterIndex==i?'selected':''}>${books[bookIndex][ci][0][0].split(':')[0]}</option>`).join('')}</select>
+            <div>${books[window.currentBookIndex][`${parseInt(chapterIndex)+1}`].map(verse=>`<p><span>${verse[0].split(':')[1]}</span> ${verse[1]}</p>`).join('')}</div>`
+    }
+    
+    window.currentBookIndex = 42
+    window.currentChapterIndex = 0
+    render(window.currentBookIndex, window.currentChapterIndex)
 }
-window.book = function(bookIndex) {
-    render(bookIndex)
-}
-function render(bookIndex) {
-    console.log('bookIndex', bookIndex)
-    const d = document
-    var body = d.querySelector('body')
-    body.innerHTML = `<select onchange="book(this.value)">${books.map((b,i)=>`<option value="${i}">${chapterTitles[i]}</option>`)}</select>`
-}
-render(0)
-console.log(books)
+load()
